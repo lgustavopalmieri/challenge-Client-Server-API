b1551098703c0e8d1b7ca01d6ce4e116fd871903
lgustavopalmieri <lgustavopalmieri@gmail.com>
2023-08-21 08:32:00 -0300

    feat: starts client

---
+++ client/client.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func GetCurrentDollar() {
+	http.HandleFunc("/", handleDollarFromServer)
+}
+
+func handleDollarFromServer(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		panic(err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lgustavopalmieri/challenge-Client-Server-API
+
+go 1.20
